68badb03937ee18a7210a103601f745232a4e79d
DenisCarriere <carriere.denis@gmail.com>
2015-04-04 04:30:10 -0400

    Modified scripts

--- 2_clip.sh
+++ 2_clip.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+export OSMFILE_IN="OSM/florida-latest.osm.pbf"
+export OSMFILE_OUT="OSM/The Villages.osm"
+
 osmosis \
-	--read-pbf file="florida-latest.osm.pbf" \
-	--bounding-box \
-		top=28.9818241 \
-		left=-82.0371536 \
-		bottom=28.8220054 \
-		right=-81.9287816 \
-	--write-xml file="The Villages.osm"
+    --read-pbf file=$OSMFILE_IN \
+    --bounding-box \
+        top=28.9818241 \
+        left=-82.0371536 \
+        bottom=28.8220054 \
+        right=-81.9287816 \
+        clipIncompleteEntities=true \
+    --write-xml file=$OSMFILE_OUT
+
