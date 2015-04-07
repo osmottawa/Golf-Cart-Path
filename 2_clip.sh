96983059eaf231c79405ff4568f7c11835ddaf7f
DenisCarriere <carriere.denis@gmail.com>
2015-04-06 20:52:50 -0400

    Clean up GolfCart path

--- 2_clip.sh
+++ 2_clip.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
 export OSMFILE_IN="florida-latest.osm.pbf"
-export OSMFILE_OUT="The-Villages.osm.pbf"
+export OSMFILE_CLIP="The-Villages-Clip.osm.pbf"
+export OSMFILE_O5M="The-Villages-Clip.o5m"
+export OSMFILE_ROADS="The-Villages-Roads.osm"
 
 osmosis \
     --read-pbf file=$OSMFILE_IN \
     --bounding-box \
-        top=28.9818241 \
-        left=-82.0371536 \
-        bottom=28.8220054 \
-        right=-81.9287816 \
+        top=29.040175 \
+        left=-82.174905 \
+        bottom=28.761252 \
+        right=-81.821970 \
         clipIncompleteEntities=true \
-    --write-pbf file=$OSMFILE_OUT
+    --write-pbf file=$OSMFILE_CLIP
 
+osmconvert $OSMFILE_CLIP \
+    -o=$OSMFILE_O5M
+
+osmfilter $OSMFILE_O5M \
+    --keep="golf_cart=yes =designated highway=path =service =residential =unclassified =tertiary =cycleway" \
+    --drop="golf_cart=no highway=footway" \
+    -o=$OSMFILE_ROADS
+
+# Final Version is PBF format
+osmosis \
+    --read-xml file=$OSMFILE_ROADS \
+    --write-pbf file=$OSMFILE_ROADS.pbf
+
+# Remove Extra files
+rm $OSMFILE_CLIP
+rm $OSMFILE_O5M
---
+++ graphhopper/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export PORT=8989
+export OSMFILE="The-Villages-Roads.osm.pbf"
+
+java -Xmx300m -jar \
+    target/golf-routes*jar \
+    config=config.properties \
+    osmreader.osm=$OSMFILE \
+    graph.location="graph-cache2" \
+    golf-cart.file="golf-cart-paths.kml" \
+    jetty.resourcebase="webapp" \
+    jetty.port=$PORT
