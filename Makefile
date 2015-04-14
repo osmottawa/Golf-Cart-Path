946933a8822cf4a434070b9eb67ee3fdeba66e4b
Denis Carriere <carriere.denis@gmail.com>
2015-04-13 22:18:56 -0400

    Clean Up repo
    Former-commit-id: f9f775e0d7c3cc591e469d7615ffdaf748d2b526

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
 init:
-	echo "hello World"
+	sudo apt-get osmosis
+	sudo apt-get install osmctools
 
+download:
+	./scripts/download.sh
+
+setup:
+	./scripts/setup.sh
+
+start:
+	./scripts/start.sh
---
+++ scripts/download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export FOLDER="~/.cache/graphhopper/"
+export DOWNLOAD="florida-latest.osm.pbf"
+export DOWNLOAD_OSMFILE_IN=$FOLDER$DOWNLOAD
+
+wget http://download.geofabrik.de/north-america/us/$DOWNLOAD -O $DOWNLOAD_OSMFILE_IN
