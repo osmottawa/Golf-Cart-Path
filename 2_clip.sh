8f4bdee464341c7f0ceabf8ec39703e3d19ee552
Denis Carriere <carriere.denis@gmail.com>
2015-04-14 01:39:52 +0000

    Restructured cache items

--- 2_clip.sh
+++ 2_clip.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# Install Dependencies
+sudo apt-get install osmctools
+
+# Define Variables
 export OSMFILE_IN="florida-latest.osm.pbf"
 export OSMFILE_CLIP="The-Villages-Clip.osm.pbf"
 export OSMFILE_O5M="The-Villages-Clip.o5m"
 export OSMFILE_ROADS="The-Villages-Roads.osm"
 
+# Clip to bounding box
 osmosis \
     --read-pbf file=$OSMFILE_IN \
     --bounding-box \
@@ -15,9 +20,11 @@ osmosis \
         clipIncompleteEntities=true \
     --write-pbf file=$OSMFILE_CLIP
 
+# Convert to O5M format for OsmFilter to use.
 osmconvert $OSMFILE_CLIP \
     -o=$OSMFILE_O5M
 
+# Filter out data, remove un-wanted tags
 osmfilter $OSMFILE_O5M \
     --keep="golf_cart=yes =designated highway=path =service =residential =unclassified =tertiary =cycleway" \
     --drop="golf_cart=no highway=footway" \
@@ -31,3 +38,4 @@ osmosis \
 # Remove Extra files
 rm $OSMFILE_CLIP
 rm $OSMFILE_O5M
+rm $OSMFILE_ROADS
