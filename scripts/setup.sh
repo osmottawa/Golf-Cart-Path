581bc11a0ed9c18b1481b060e29092aed3e953a2
Denis Carriere <carriere.denis@gmail.com>
2015-04-14 22:30:43 -0400

    Update Golf cart Schema

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,8 +28,8 @@ osmconvert $OSMFILE_CLIP \
 
 # Filter out data, remove un-wanted tags
 osmfilter $OSMFILE_CLIP_O5M \
-    --keep="golf_cart=yes =designated highway=path =service =residential =unclassified =tertiary =cycleway" \
-    --drop="golf_cart=no highway=footway" \
+    --keep="golf_cart=yes =designated highway=path =service =residential =unclassified =cycleway" \
+    --drop="golf_cart=no highway=footway golf=cartpath" \
     -o=$OSMFILE_FINAL
 
 # Final Version is PBF format
