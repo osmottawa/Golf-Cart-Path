4ffe8ffbc8c7bd4c06be0cc893dc52dd9f1b0c19
Denis Carriere <carriere.denis@gmail.com>
2015-04-19 08:22:32 +0000

    Add PID kill in setup
    Former-commit-id: c723ef456ead62ba7f4f44b3660b0475feefe140

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,11 @@ export PORT=8888
 export OSMFILE="/home/ubuntu/.cache/graphhopper/The-Villages-Final.osm.pbf"
 export KMLFILE="/home/ubuntu/.cache/graphhopper/golf-cart-paths.kml"
 
+# Kill any existing Graphhopper instance
+PID=$(cat pid.file)
+kill -9 $PID
+
+# Run Graphhopper
 java -Xmx300m -jar \
     target/golf-routes*jar \
     config=config.properties \
@@ -11,4 +16,7 @@ java -Xmx300m -jar \
     graph.location="graph-cache2" \
     golf-cart.file=$KMLFILE \
     jetty.resourcebase="webapp" \
-    jetty.port=$PORT
+    jetty.port=$PORT &
+
+# Store PID to kill later
+echo $! > pid.file
