1c5f4eeffa37e5d938a30efe19fdf71802e93091
DenisCarriere <carriere.denis@gmail.com>
2015-04-19 04:48:16 -0400

    Clean scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
+export USER_HOME=$(eval echo ~${SUDO_USER})
 export PORT=8888
-export OSMFILE="/home/ubuntu/.cache/graphhopper/The-Villages-Final.osm.pbf"
-export KMLFILE="/home/ubuntu/.cache/graphhopper/golf-cart-paths.kml"
+export OSMFILE=$USER_HOME"/.cache/graphhopper/The-Villages-Final.osm.pbf"
+export KMLFILE=$USER_HOME"/.cache/graphhopper/golf-cart-paths.kml"
+export GRAPH_CACHE=$USER_HOME"/graphhopper/graph-cache"
+cd $USER_HOME"/Github/graphhopper"
 
 # Kill any existing Graphhopper instance
 PID=$(cat pid.file)
 kill -9 $PID
+
+# Remove existing Cache
+rm -r $GRAPH_CACHE
 
 # Run Graphhopper
 java -Xmx300m -jar \
     target/golf-routes*jar \
     config=config.properties \
     osmreader.osm=$OSMFILE \
-    graph.location="graph-cache2" \
+    graph.location=$GRAPH_CACHE \
     golf-cart.file=$KMLFILE \
     jetty.resourcebase="webapp" \
     jetty.port=$PORT &
