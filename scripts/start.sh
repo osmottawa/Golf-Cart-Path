79e10cbeba80a26677527caadad6ea5bad955da0
Denis Carriere <carriere.denis@gmail.com>
2015-06-02 23:53:01 +0000

    Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,7 @@
 
 export USER_HOME=$(eval echo ~${SUDO_USER})
 export JETTY_PORT=8888
-export OSMFILE=$USER_HOME"/.cache/graphhopper/florida-latest.osm.pbf"
-export GRAPH_CACHE=$USER_HOME"/.cache/graphhopper/graph-cache"
+export OSMFILE=$USER_HOME"/.cache/graphhopper/The-Villages.osm.pbf"
 cd "./graphhopper"
 
 # Kill any existing Graphhopper instance
@@ -11,5 +10,4 @@ PID=$(ps aux | grep golf-routes | grep -v grep | awk '{print $2}')
 pkill java
 
 # Run Graphhopper
-rm -r $GRAPH_CACHE
- ./graphhopper.sh web $OSMFILE
+nohup ./graphhopper.sh web $OSMFILE
